b52d9e72670a115d5fef388e291b4886d5573b91
Mohanarangan Muthukumar <mohan@extrasalt.org>
2016-01-23 14:08:17 +0000

    Added POST method to generator.go

--- generator.go
+++ generator.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
@@ -15,6 +16,15 @@ func GeneratorHandler(w http.ResponseWriter, r *http.Request) {
 			log.Println("Failed to parse files")
 		}
 		t.Execute(w, nil)
+	
+	case "POST":
+		if err := r.ParseForm(); err != nil {
+			log.Println("Failed to get post values")
+		}
+
+		rawText := r.PostFormValue("desc")
+		fmt.Println(rawText)
+		fmt.Fprintln(w, rawText)
 
 	default:
 		http.Error(w, "Method not allowed", 405)
