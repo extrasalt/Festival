ff23b197287218c2db599ace1726a14879319572
Mohanarangan Muthukumar <mohan@extrasalt.org>
2016-01-23 17:40:19 +0000

    Added static dir and index.html

--- main.go
+++ main.go
@@ -21,7 +21,8 @@ func main() {
 	}
 
 	pageCol = myDB.Use("Pages")
-
+	
+	http.Handle("/", http.FileServer(http.Dir("./public")))
 	http.HandleFunc("/gen", GeneratorHandler)
 	http.HandleFunc("/p/", PageHandler)
 	http.HandleFunc("/new", NewPageHandler)
